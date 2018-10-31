a550726d20da513b84f2b14a21e8eecea4430bd9
ArthurInTheShell <32620957+ArthurInTheShell@users.noreply.github.com>
2018-10-31 16:32:28 -0400

    Add prime check and GCD LCM to binder

---
+++ TheBinder/GCD.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String...Args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+        int c = gcd(n,m);
+        int d = n*m/c;
+        System.out.println("gcd: "+c);
+        System.out.println("lcm: "+d);
+    }
+
+    private static int gcd(int u, int v) {
+        return u%v==0?v:gcd(v,u%v);
+    }
+
+}
